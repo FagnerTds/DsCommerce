4b99526832f402a448647c2200bf7971ae678f4d
Fagner Tranquilino <fagnertds@gmail.com>
2025-01-31 16:36:32 -0300

    Get Product paged

--- src/main/java/com/fagnertds/DsCommerce/services/ProductService.java
+++ src/main/java/com/fagnertds/DsCommerce/services/ProductService.java
@@ -3,8 +3,9 @@ package com.fagnertds.DsCommerce.services;
 import com.fagnertds.DsCommerce.dto.ProductDTO;
 import com.fagnertds.DsCommerce.entities.Product;
 import com.fagnertds.DsCommerce.repositories.ProductRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,5 +23,10 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
