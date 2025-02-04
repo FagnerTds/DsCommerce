38371401948a3f3aed9417c143eee1680480526f
Fagner Tranquilino <fagnertds@gmail.com>
2025-02-04 12:14:29 -0300

    metodo post e ResponseEntity

--- src/main/java/com/fagnertds/DsCommerce/services/ProductService.java
+++ src/main/java/com/fagnertds/DsCommerce/services/ProductService.java
@@ -29,4 +29,15 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
     }
+
+    public ProductDTO insert(ProductDTO productDTO) {
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setImgUrl(productDTO.getImgUrl());
+        product.setPrice(productDTO.getPrice());
+
+        product = repository.save(product);
+        return new ProductDTO(product);
+    }
 }
