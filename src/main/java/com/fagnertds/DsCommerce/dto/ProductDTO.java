242fbe6696dae7a79e708d77d1a29558ed61b170
Fagner Tranquilino <fagnertds@gmail.com>
2025-02-06 15:13:43 -0300

    bean validation

--- src/main/java/com/fagnertds/DsCommerce/dto/ProductDTO.java
+++ src/main/java/com/fagnertds/DsCommerce/dto/ProductDTO.java
@@ -2,12 +2,20 @@ package com.fagnertds.DsCommerce.dto;
 
 import com.fagnertds.DsCommerce.entities.Product;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+    @Size (min = 3, max = 120, message = "Nome precisa ter de 3 a 120 caracteres")
+    @NotBlank(message = "campo requerido")
     private String name;
+    @Size (min = 10, message = "Descrição deve ter no mínimo 10 caracteres")
+    @NotBlank(message = "campo requerido")
     private String description;
+    @Positive(message = "campo deve conter apenas números positivos")
     private Double price;
     private String imgUrl;
 
